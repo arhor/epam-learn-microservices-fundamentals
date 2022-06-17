c2912bdc9fa32d0e6509e62f095de623f2689966
Maskim Buryshynets <v-mburyshynets@expediagroup.com>
2022-06-17 14:59:47 +0300

    refactoring

--- resource-processor/src/main/kotlin/com/epam/learn/microservices/fundamentals/resource/processor/config/RestClientConfig.kt
+++ resource-processor/src/main/kotlin/com/epam/learn/microservices/fundamentals/resource/processor/config/RestClientConfig.kt
@@ -1,17 +1,20 @@
 package com.epam.learn.microservices.fundamentals.resource.processor.config
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.web.client.RestTemplate
 
 @Configuration(proxyBeanMethods = false)
+@Import(RestTemplateAutoConfiguration::class)
 class RestClientConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+    fun defaultRestTemplate(builder: RestTemplateBuilder): RestTemplate {
         return builder.build()
     }
 }
