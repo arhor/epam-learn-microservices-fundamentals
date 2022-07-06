33a60e860f71e4ccc6e6e39592f7bfe00f40485b
Maskim Buryshynets <v-mburyshynets@expediagroup.com>
2022-07-06 16:02:18 +0300

    added integrationTest task

---
+++ app-song-service/src/test/kotlin/com/epam/learn/microservices/fundamentals/song/service/service/impl/SongServiceImplTest.kt
@@ -0,0 +1,10 @@
+package com.epam.learn.microservices.fundamentals.song.service.service.impl
+
+import org.junit.jupiter.api.Test
+
+internal class SongServiceImplTest {
+
+    @Test
+    fun `should not fail`() {
+    }
+}
