72de8267769078cf020bad52e09c46d79c67e60a
Maskim Buryshynets <v-mburyshynets@expediagroup.com>
2022-07-07 20:10:40 +0300

    cypress test refactored

--- tests-e2e/cypress/e2e/create-resource.cy.js
+++ tests-e2e/cypress/e2e/create-resource.cy.js
@@ -1,8 +1,13 @@
 describe('resources API test', () => {
     it('should create new resource', () => {
+        // given
         cy.request('GET', 'http://localhost:5001/api/resources/1').as('resourceByIdRequest');
 
-        cy.get('@resourceByIdRequest').then(response => {
+        // when
+        const apiCall = cy.get('@resourceByIdRequest');
+
+        // then
+        apiCall.then(response => {
             expect(response.status).to.eq(200);
         });
     });
